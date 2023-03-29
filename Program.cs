847631b504b78719c30f8efc8677401eb739cc31
Guilherme Tonon <gui.tonon.03@gmail.com>
2023-03-29 18:40:20 -0300

    estudos csharp

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using CSharp.Models;
+
+int a = 90;
+
+int b = 3;
+
+bool fe = a > b;
+
+if(fe)
+    Console.WriteLine(24);
+else
+    Console.WriteLine("nao");
+
+
+// double d = 1.8349999;
+
+// // Pessoa p = new Pessoa();
+
+// // p.Nome = "Guilherme";
+// // p.Idade = 20;
+// // p.Apresentar();
+
+// Console.WriteLine(d.ToString("0.0"));
