8eb6750956c9047c9ec5a84725db9d3e464c7f93
Guilherme Tonon <gui.tonon.03@gmail.com>
2023-03-29 22:46:43 -0300

    estudos CSharp

--- Program.cs
+++ Program.cs
@@ -1,23 +1,8 @@
 ﻿using CSharp.Models;
 
-int a = 90;
 
-int b = 3;
+Calculadora c = new Calculadora();
 
-bool fe = a > b;
-
-if(fe)
-    Console.WriteLine(24);
-else
-    Console.WriteLine("nao");
-
-
-// double d = 1.8349999;
-
-// // Pessoa p = new Pessoa();
-
-// // p.Nome = "Guilherme";
-// // p.Idade = 20;
-// // p.Apresentar();
-
-// Console.WriteLine(d.ToString("0.0"));
+c.RaizQuadrada(49);
+c.RaizQuadrada(144);
+c.RaizQuadrada(-2);
