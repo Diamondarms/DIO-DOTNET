6ce13d7d47df09fb8dfa554fc4bbc246bbe854de
Guilherme Tonon <gui.tonon.03@gmail.com>
2023-05-08 17:00:20 -0300

    fundamentos

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -2,12 +2,23 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
-int numero = 263;
-bool par = false;
+int qt = int.Parse(Console.ReadLine());
+        string[] v = new string[2];
+        // TODO: Crie as outras condições necessárias para a resolução do desafio:
+        for (int i = 0; i < qt; i++)
+        {
+            v = Console.ReadLine().Split(" ");
+            string a = v[0];
+            string b = v[1];
+            
+            if (b.Length > a.Length)
+                Console.WriteLine("nao encaixa");
+            else if (a.EndsWith(b))
+                Console.WriteLine("encaixa");
+            else
+                Console.WriteLine("nao encaixa");
 
-par = numero.EhPar();
-
-Console.WriteLine(par);
+        }
 
 
 
