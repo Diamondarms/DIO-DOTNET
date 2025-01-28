edfa0358e55fb202ca37db72464e887d7f87ce64
Diamondarms <gui.tonon.03@gmail.com>
2025-01-27 22:57:22 -0300

    migrations

--- ModuloAPI/obj/Debug/net9.0/ModuloAPI.AssemblyInfo.cs
+++ ModuloAPI/obj/Debug/net9.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77c50d5d6cc7dea3413ef36123857b5304e7ed8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f34341462eb3db37b8c8b33e709af6f957b157e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
